6638964b4126d48d69d5d2e271392a33515ec8de
Bryan <bryanoliverh@gmail.com>
2021-09-19 18:49:57 +0700

    last

--- libraryengineering/settings.py
+++ libraryengineering/settings.py
@@ -138,5 +138,4 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'holiverbryan@gmail.com'
-EMAIL_HOST_PASSWORD = 'chuzzle123'
+
